53b0ef0240a7c8dcc294ff4af87659e8f26a678e
Ken Sodemann <ken@ionic.io>
2019-03-26 11:51:57 -0500

    feat(app): add a child page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ChildOnePage } from '../child-one/child-one';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(private navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  gotoChild() {
+    this.navCtrl.push(ChildOnePage);
   }
-
 }
